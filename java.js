73fc36773bb6e641387aed65cf1d4b00ae1f8c99
om omar <124066052+nedalalshami@users.noreply.github.com>
2024-12-16 10:45:44 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,9 +1,8 @@
-function returnVideo1(){
-v1_back.innerHTML='<iframe width="250" height="300" src="https://www.youtube.com/embed/ZUP-hoB8DvQ" title="كيف أستخدم المتغيرات في css مع خاصيةcalc( ) لتنسيق العناصر بسهولة" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
-    }
-
-function returnVideo2(){
-    v2_back.innerHTML='<iframe width="250"height="300" src="https://www.youtube.com/embed/Lfnmo5zTaSw" title="أكثر من فكرة ب 5 دقائق فقط  HTML &amp; CSS" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
-}
-
+  function returnVideo1(){
+        v1_back.innerHTML='<iframe width="250" height="300" src="https://www.youtube.com/embed/ZUP-hoB8DvQ" title="كيف أستخدم المتغيرات في css مع خاصيةcalc( ) لتنسيق العناصر بسهولة" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+            }
+        
+        function returnVideo2(){
+            v2_back.innerHTML='<iframe width="250"height="300" src="https://www.youtube.com/embed/Lfnmo5zTaSw" title="أكثر من فكرة ب 5 دقائق فقط  HTML &amp; CSS" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+        }
 
