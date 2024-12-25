529755fefbd0aee6872c003a2c502ec4f1506045
om omar <124066052+nedalalshami@users.noreply.github.com>
2024-12-25 11:21:54 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
   function returnVideo1(){
-        v1_back.innerHTML='<iframe width="250" height="300" src="https://www.youtube.com/embed/ZUP-hoB8DvQ" title="كيف أستخدم المتغيرات في css مع خاصيةcalc( ) لتنسيق العناصر بسهولة" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+        v1_back.innerHTML='<iframe width="250" height="300" src="https://www.youtube.com/embed/-0tjxfmp3b4" title="شرح addEventListener /إنشاء واستدعاء الوظيفة ضمن ملف الجافا سكريت" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
             }
         
         function returnVideo2(){
